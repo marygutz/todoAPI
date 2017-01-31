3da776496c44d6252fff29a5a55a5f4182886698
Mary Licanin <mary@Marys-MacBook-Pro.local>
2017-01-30 21:58:51 -0600

    add /todos and /todos:id

--- server.js
+++ server.js
@@ -2,8 +2,48 @@ var express = require('express')
 var app = express()
 var PORT = process.env.PORT || 3000
 
+// model in collection
+var todos = [{
+  id: 1,
+  description: 'meet mom for lunch',
+  completed: false
+},
+  {
+    id: 2,
+    description: 'go to market',
+    completed: false
+  },
+  {
+    id: 3,
+    description: 'do laundry',
+    completed: true
+  }
+]
+
 app.get('/', function (req, res) {
   res.send('todo API root')
+})
+
+// get list of todos
+app.get('/todos', function (req, res) {
+  res.json(todos)
+})
+
+app.get('/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id, 10)
+  var matched
+
+  todos.forEach(function (todo) {
+  	if (todoId === todo.id) {
+  		matched = todo
+  		}
+  })
+
+  	if (matched) {
+    	res.json(matched)
+  	} else {
+  		res.status(404).send()
+  	}
 })
 
 app.listen(PORT, function () {
