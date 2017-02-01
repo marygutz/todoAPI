47379714096999fba7835b54f70e64c82f7ab628
Mary Licanin <mary@Marys-MacBook-Pro.local>
2017-01-31 22:45:00 -0600

    add PUT todos

--- server.js
+++ server.js
@@ -73,6 +73,33 @@ app.delete('/todos/:id', function (req, res) {
   	}
 })
 
+// PUT
+app.put('/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id, 10)
+  var matched = _.findWhere(todos, {id: todoId})
+  var body = _.pick(req.body, 'description', 'completed')
+  var validAttributes = {}
+
+  if (!matched) {
+  	return status(404).send()
+  }
+
+  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+  	validAttributes.completed = body.completed
+  } else if (body.hasOwnProperty('completed')) {
+  	return res.status(400).send()
+  }
+
+  if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+  	validAttributes.description = body.description
+  } else if (body.hasOwnProperty('description')) {
+  	return res.status(400).send()
+  }
+
+  _.extend(matched, validAttributes)
+  res.json(matched)
+})
+
 app.listen(PORT, function () {
   console.log('express listening on port ' + PORT + '!')
 })
