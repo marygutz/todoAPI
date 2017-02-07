22704701cdf7f37da12c7c00a95dc92919d1c1da
Mary Licanin <mary@Marys-MacBook-Pro.local>
2017-02-06 19:59:36 -0600

    add postgres for prod

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 var Sequelize = require('sequelize')
-var sequelize = new Sequelize(undefined, undefined, undefined, {
+var env = process.env.NODE_ENV || 'development'
+var sequelize
+
+if (env === 'production') { // heroku
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres'
+  })
+} else { // local
+  sequelize = new Sequelize(undefined, undefined, undefined, {
 	// use sqlite db
-  'dialect': 'sqlite',
+    'dialect': 'sqlite',
 	// where to store db
-  'storage': __dirname + '/data/dev-todo-api.sqlite'
-})
+    'storage': __dirname + '/data/dev-todo-api.sqlite'
+  })
+}
 
 var db = {}
 
